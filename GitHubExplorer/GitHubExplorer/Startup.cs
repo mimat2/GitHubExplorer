125a208827ddca9cb6a656d405ba57121a2d106d
mimat2 <michal.matuszewski@cybercom.com>
2017-09-05 14:54:10 +0200

    Initial project commit

---
+++ GitHubExplorer/GitHubExplorer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitHubExplorer.Startup))]
+namespace GitHubExplorer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
